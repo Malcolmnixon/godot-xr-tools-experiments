1f7a2fb0e8deefdc3b889259353f52c8993a9143
Malcolm Nixon <malcolm.nixon@gmail.com>
2022-01-22 13:23:32 -0500

    Modified Object_pickable script so the user can specify their own material. Added table of pickable objects.

--- addons/godot-xr-tools/objects/Object_pickable.gd
+++ addons/godot-xr-tools/objects/Object_pickable.gd
@@ -7,6 +7,7 @@ class_name XRToolsPickable
 export (bool) var press_to_hold = true
 export (bool) var reset_transform_on_pickup = true
 export (NodePath) var highlight_mesh_instance = null
+export (Material) var highlight_material = null
 export  (int, LAYERS_3D_PHYSICS) var picked_up_layer = 0
 
 # Remember some state so we can return to it when the user drops the object
@@ -14,8 +15,7 @@ onready var original_parent = get_parent()
 onready var original_collision_mask = collision_mask
 onready var original_collision_layer = collision_layer
 var original_mode
-
-onready var highlight_material = preload("res://addons/godot-xr-tools/materials/highlight.tres")
+var highlight_material_active = null
 var original_materials = Array()
 var highlight_mesh_instance_node : MeshInstance = null
 
@@ -42,7 +42,7 @@ func _update_highlight():
 		# if we can find a node remember which materials are currently set on each surface
 		for i in range(0, highlight_mesh_instance_node.get_surface_material_count()):
 			if closest_count > 0:
-				highlight_mesh_instance_node.set_surface_material(i, highlight_material)
+				highlight_mesh_instance_node.set_surface_material(i, highlight_material_active)
 			else:
 				highlight_mesh_instance_node.set_surface_material(i, original_materials[i])
 	else:
@@ -123,6 +123,11 @@ func let_go(p_linear_velocity = Vector3(), p_angular_velocity = Vector3()):
 		by_controller = null
 
 func _ready():
+	# Load default highlight material if not specified
+	highlight_material_active = highlight_material;
+	if !highlight_material_active:
+		highlight_material_active = preload("res://addons/godot-xr-tools/materials/highlight.tres")
+		
 	if highlight_mesh_instance:
 		# if we have a highlight mesh instance selected obtain our node
 		highlight_mesh_instance_node = get_node(highlight_mesh_instance)
